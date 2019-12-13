a331577e9cb6e205e3164a608cafacca171652f8
sanjaysathu <snjsathu@gmail.com>
2019-12-14 04:50:22 +0530

    Updates

---
+++ precache-manifest.f58d5bf4a3c9e94658fcfdec0e049e15.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f8dbef9745e844da42f94ee0e1a2d4",
+    "url": "/IntugineTask/index.html"
+  },
+  {
+    "revision": "f99171fdcaf006cc040f",
+    "url": "/IntugineTask/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e9b7f72880f255821360",
+    "url": "/IntugineTask/static/css/main.29656757.chunk.css"
+  },
+  {
+    "revision": "f99171fdcaf006cc040f",
+    "url": "/IntugineTask/static/js/2.e94c83c4.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/IntugineTask/static/js/2.e94c83c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9b7f72880f255821360",
+    "url": "/IntugineTask/static/js/main.fd6d4906.chunk.js"
+  },
+  {
+    "revision": "7ab964b7857b471e27b7",
+    "url": "/IntugineTask/static/js/runtime-main.5af4c943.js"
+  },
+  {
+    "revision": "85f9fdcc34b168fd80fa48babd0e1b99",
+    "url": "/IntugineTask/static/media/destination.85f9fdcc.svg"
+  },
+  {
+    "revision": "877653aeb01976fd7edf924c661eacc2",
+    "url": "/IntugineTask/static/media/logo.877653ae.svg"
+  },
+  {
+    "revision": "52b703a8be7195bf860d5e89cdc1ec6e",
+    "url": "/IntugineTask/static/media/profile.52b703a8.svg"
+  },
+  {
+    "revision": "1f5bfd3e805919a486ffb8e1ab44785a",
+    "url": "/IntugineTask/static/media/warehouse.1f5bfd3e.svg"
+  }
+]);
