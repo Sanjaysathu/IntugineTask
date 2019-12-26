6459f59c20f3727b18823c6639f0658ac0a6ff0a
sanjaysathu <snjsathu@gmail.com>
2019-12-26 10:39:08 +0530

    Updates

---
+++ precache-manifest.ba0252dfac0fdddc35750b13bdce0581.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b047a7b6439b9aedd2b93392467c51d",
+    "url": "/ShipmentDashboard/index.html"
+  },
+  {
+    "revision": "fb71e88b17d34b501c6f",
+    "url": "/ShipmentDashboard/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7d74d1d0ee48bbba2bad",
+    "url": "/ShipmentDashboard/static/css/main.9c4ff9f0.chunk.css"
+  },
+  {
+    "revision": "fb71e88b17d34b501c6f",
+    "url": "/ShipmentDashboard/static/js/2.4ce69ad4.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/ShipmentDashboard/static/js/2.4ce69ad4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d74d1d0ee48bbba2bad",
+    "url": "/ShipmentDashboard/static/js/main.5f200316.chunk.js"
+  },
+  {
+    "revision": "dfe5073bb8760a159399",
+    "url": "/ShipmentDashboard/static/js/runtime-main.24536650.js"
+  },
+  {
+    "revision": "85f9fdcc34b168fd80fa48babd0e1b99",
+    "url": "/ShipmentDashboard/static/media/destination.85f9fdcc.svg"
+  },
+  {
+    "revision": "52b703a8be7195bf860d5e89cdc1ec6e",
+    "url": "/ShipmentDashboard/static/media/profile.52b703a8.svg"
+  },
+  {
+    "revision": "1f5bfd3e805919a486ffb8e1ab44785a",
+    "url": "/ShipmentDashboard/static/media/warehouse.1f5bfd3e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/IntugineTask/precache-manifest.2f232f6ce7eded04de4b379febf2b106.js"
+  "/ShipmentDashboard/precache-manifest.ba0252dfac0fdddc35750b13bdce0581.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IntugineTask/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShipmentDashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
